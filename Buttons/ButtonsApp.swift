be22992af2398f7e9ac64bb9e27fd4994e8b2b03
Chris Carey <crc2220@gmail.com>
2023-01-01 10:51:53 -0500

    Initial Commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Chris Carey on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
